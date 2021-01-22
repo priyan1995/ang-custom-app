eff382edd1049ec6e2438611420cc8ac85f1a009
priyan <45042030+priyan1995@users.noreply.github.com>
2021-01-23 00:30:44 +0530

    environment url set

---
+++ CRUD-user/src/app/_services/users/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private _http: HttpClient) {
+    addUser(user){
+      
+    }
+  }
+}
--- CRUD-user/src/environments/environment.ts
+++ CRUD-user/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appName: "Sample Application",
+  apiUrl: 'localhost/TestProj'
 };
 
 /*
